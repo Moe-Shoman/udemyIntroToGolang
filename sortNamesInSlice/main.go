99996b7bbbde700fa9c806fe4eae6a433d691bd3
Yasin Shuman <yshuman@loyalloops.com>
2017-07-16 00:54:00 -0700

    learned abotu interfaces and how to sort a slice of strings

---
+++ sortNamesInSlice/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//take a slice and sort it.
+
+type people []string
+
+func (p people) Len() int           { return len(p) }
+func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p people) Less(i, j int) bool { return p[i] < p[j] }
+
+func main() {
+	family := people{"yasin", "awni", "dena", "dunya", "mosho", "samir", "diana", "delal"}
+	fmt.Println(family)
+	sort.Sort(family)
+	fmt.Println(family) //this is the result of them sorted
+	family.Swap(0, 7)   //i wanted to play with the methods of the interface
+	fmt.Println(family)
+	a := family.Len()
+	fmt.Println(a)
+}
